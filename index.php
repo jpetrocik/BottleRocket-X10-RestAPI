39c877153e6d4c33d1695a9cb101a0a81aab8ca3
John Petrocik <john@petrocik.net>
2017-01-14 01:08:13 -0800

    Added house and device specific endpoints

--- index.php
+++ index.php
@@ -4,20 +4,66 @@
 //G	1	living room ceilling lights
 //G	2	landscape lights
 
-//light on lock file created when on, delete when off
-$lv_lock_file="/tmp/lv.light";
+//lock file created when on, delete when off,
+//used to indicate current status
+//possible to get out of sync when lights are
+//manually turned off
+$x10_lock_dir = "/tmp/x10/";
 
-function lightsOff($lock_file) {
-        exec('/usr/bin/br -x /dev/ttyS0 G ALL_OFF');
-        unlink($lock_file);
+$default_house = "G";
+
+openlog("x10", LOG_PID | LOG_PERROR, LOG_LOCAL0);
+
+function sendCommand($args) {
+    exec('/usr/bin/br -x /dev/ttyS0 ' . $args);
 }
 
-function lightsOn($lock_file) {
-        exec('/usr/bin/br -x /dev/ttyS0 G ALL_ON');
-        touch($lock_file);
+//turns on all devices
+function allOff($house) {
+	global $x10_lock_dir;
+
+	syslog(LOG_INFO, "Turning all devices off at $house");
+    sendCommand($house . ' ALL_OFF');
+    $files = glob($x10_lock_dir . "*.on");
+    foreach ($files as $value) {
+	    unlink($value);
+    }
 }
 
-function checkLightStatus($lock_file){
+//turns off all devices
+function allOn($house) {
+	global $x10_lock_dir;
+
+	syslog(LOG_INFO, "Turning all devices on at $house");
+    sendCommand($house . ' ALL_ON');
+	for ($i = 1; $i < 16; $i++) {
+	    touch($x10_lock_dir . $house . $i . '.on');
+	}
+}
+
+//turns off a single device
+function deviceOff($house, $device) {
+	global $x10_lock_dir;
+
+	syslog(LOG_INFO, "Turning device $house $device off");
+    sendCommand($house . $device . ' OFF');
+    unlink($x10_lock_dir . $house . $device . '.on');
+}
+
+//turns on a single device
+function deviceOn($house, $device) {
+	global $x10_lock_dir;
+
+	syslog(LOG_INFO, "Turning device $house $device on");
+    sendCommand($house . $device . ' ON');
+    touch($x10_lock_dir . $house . $device . '.on');
+}
+
+
+function checkDeviceStatus($house, $device){
+	global $x10_lock_dir;
+
+	$lock_file = $x10_lock_dir . $house . $device . '.on';
     if (file_exists($lock_file)){
 
             //assume lights are off if file is old
@@ -31,25 +77,43 @@ function checkLightStatus($lock_file){
 }
 
 $cmd = $_GET["cmd"];
-if ($cmd === "ALL_OFF") {
-    lightsOff($lv_lock_file);
-	$light_on = false;
-} elseif ($cmd === "ALL_ON") {
-    lightsOn($lv_lock_file);
-	$light_on = true;
-} elseif ($cmd === "STATUS") {
-    $light_on = checkLightStatus($lv_lock_file);
-} else {  //toggle lights
-    $light_on = checkLightStatus($lv_lock_file);
 
-    if ($light_on) {
-        lightsOff($lv_lock_file);
+$device = $_GET["device"];
+if (!isset($device))
+	$device = 1;
+
+$house = $_GET["house"];
+if (!isset($house))
+	$house = $default_house;
+
+
+if ($cmd === "ALL_OFF") {
+    allOff($house);
+	$status = false;
+} elseif ($cmd === "ALL_ON") {
+    allOn($house);
+	$status = true;
+} elseif ($cmd === "OFF") {
+    deviceOff($house, $device);
+	$status = false;
+} elseif ($cmd === "ON") {
+    deviceOn($house, $device);
+	$status = true;
+} elseif ($cmd === "STATUS") {
+    $status = checkDeviceStatus($house, $device);
+} else {  //toggle lights
+    $status = checkDeviceStatus($house, $device);
+
+    if ($status) {
+        deviceOff($house, $device);
     } else {
-        lightsOn($lv_lock_file);
+        deviceOn($house, $device);
     }
-    $light_on = !$light_on;
+    $status = !$status;
 }
 
-$results = array('light' => $light_on?"on":"off");
+closelog();
+
+$results = array('status' => $status?"on":"off");
 echo json_encode($results);
 ?>
